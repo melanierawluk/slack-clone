993f95b01f276adfc73ae038e060908a15fe0247
Melanie <melanie.rawluk@gmail.com>
2024-09-19 17:23:09 -0600

    add workspace sidebar, header

---
+++ src/app/workspace/[workspaceId]/WorkspaceHeader.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { ChevronDown } from "lucide-react";
+
+
+interface WorkspaceHeaderProps {
+    workspace: Doc<"workspaces">
+}
+
+export default function WorkspaceHeader({ workspace }: WorkspaceHeaderProps) {
+
+    return (
+        <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="transparent"
+                        className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+                    >
+                        <span className="truncate">{workspace.name}</span>
+                        <ChevronDown className="size-4 ml-1 shrink-0" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="start" className="w-64">
+                    <DropdownMenuItem
+                        className="cursor-pointer capitalize"
+                    >
+                        <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2">
+                            {workspace.name.charAt(0).toUpperCase()}
+                        </div>
+
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+
+    );
+}
--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import Toolbar from "./Toolbar";
 import Sidebar from "./Sidebar";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import WorkspaceSidebar from "./WorkspaceSidebar";
 
 
 interface WorkspaceIdLayoutProps {
@@ -16,7 +18,24 @@ export default function WorkspaceLayout({ children }: WorkspaceIdLayoutProps) {
             <Toolbar />
             <div className="flex h-[calc(100vh-40px)]">
                 <Sidebar />
-                {children}
+                <ResizablePanelGroup
+                    direction="horizontal"
+                    autoSaveId="workspace-layout"
+                >
+                    <ResizablePanel
+                        defaultSize={20}
+                        minSize={11}
+                        className="bg-[#5e2c5f]"
+                    >
+                        <WorkspaceSidebar />
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel
+                        minSize={20}
+                    >
+                        {children}
+                    </ResizablePanel>
+                </ResizablePanelGroup>
             </div>
         </section>
     );
