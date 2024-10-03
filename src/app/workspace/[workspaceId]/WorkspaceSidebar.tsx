d544b9418d587263fc12ac3f8d731bdf0e93986a
Melanie <melanie.rawluk@gmail.com>
2024-10-03 13:00:47 -0600

    add sidebar item

--- src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
+++ src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
@@ -1,8 +1,10 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader } from "lucide-react";
 import WorkspaceHeader from "./WorkspaceHeader";
+
+import { AlertTriangle, Loader, MessageSquareTextIcon, SendHorizonalIcon } from "lucide-react";
+import SidebarItem from "./SidebarItem";
 
 
 interface WorkspaceSidebarProps { }
@@ -34,6 +36,20 @@ export default function WorkspaceSidebar({ }: WorkspaceSidebarProps) {
     return (
         <div className="flex flex-col bg-[#5e2c5f] h-full">
             <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
+            <div className="flex flex-col px-2 mt-3">
+                <SidebarItem
+                    label="Threads"
+                    icon={MessageSquareTextIcon}
+                    id="threads"
+                />
+                <SidebarItem
+                    label="Drafts & Sent"
+                    icon={SendHorizonalIcon}
+                    id="drafts"
+                />
+
+            </div>
         </div>
+
     );
 }
